b2bef547b0e1f6dc1a853bb7f46dc90f76862f1c
Luiz Pantuzzo <pantuzzoalves@gmail.com>
2021-10-01 08:31:31 -0300

    validacao customizada: email nao repetido na insercao de Cliente

--- src/main/java/com/example/demo/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/example/demo/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,21 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.demo.domain.Cliente;
 import com.example.demo.domain.enums.TipoCliente;
 import com.example.demo.dto.ClienteNewDTO;
+import com.example.demo.repositories.ClienteRepository;
 import com.example.demo.resources.exception.FieldMessage;
 import com.example.demo.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo; 
+	
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 		
@@ -31,6 +40,10 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		}
 		
 		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if(aux != null) {
+			list.add(new FieldMessage("email", "Email ja existente"));
+		}
 		
 		// inclua os testes aqui, inserindo erros na lista
 
